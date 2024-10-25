d41709fc7eabaa12b6fddc60a1768afa2cf5d5ec
Aadhithya1806 <aadhithya1837@gmail.com>
2024-10-25 11:04:18 +0530

    Added Basic Structure,Landing Page and Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-function App() {
-  return (
-    <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
-=======
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 function App() {
@@ -13,7 +5,6 @@ function App() {
     <>
       <Header></Header>  
       <Meals></Meals>
->>>>>>> food_order_app_v0
     </>
   );
 }
